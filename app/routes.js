4eb5cbf43b257188f7709d91dc69cb62b15370f2
Naomi W <naomi.wilcock@digital.homeoffice.gov.uk>
2018-05-17 15:11:47 +0100

    Add a juggling equipment page

--- app/routes.js
+++ app/routes.js
@@ -20,4 +20,17 @@ router.post('/juggling-balls-answer', function (req, res) {
   }
 })
 
+router.post('/juggling-equipment-answer', function (req, res) {
+  //make a variable and give it a value from 'juggling-equipment'
+  var jugglingEquipment = req.session.data ['juggling-equipment']
+  //check whether the variable matches a condition
+  if (jugglingEquipment == "Batons") {
+    //send user to next page
+    res.redirect ('/separate-licence')
+  } else {
+    //send user to ineligible pages
+    res.redirect ('/check-your-answers')
+  }
+})
+
 module.exports = router
